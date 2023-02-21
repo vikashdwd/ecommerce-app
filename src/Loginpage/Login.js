5cb97deffd3f44973acf34b0989f46b12253e898
shivesh <vikashdwd428@gmail.com>
2023-02-22 00:23:50 +0530

    added state forgot

--- src/Loginpage/Login.js
+++ src/Loginpage/Login.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Login.css';
 import Password from './Password';
@@ -8,28 +9,62 @@ import Signup from './Signup';
 
 function Login(){
 
+	const [forgot,setForgot]= useState(0);
 
-    return (
+
+
+	if({forgot}>=1)
+	{
+		console.log("forgot");
+		return(
+
 
 <div class= "login_page">
 
-		<input type="checkbox" id="chk" aria-hidden="true"/>
+<input type="checkbox" id="chk" aria-hidden="true"/>
 <Signup/>
 <div className="login">
-
 <form>
-					<label for="chk" aria-hidden="true">Login</label>
-					<input type="email" name="email" placeholder="Email" required=""/>
-					<Password/>
-
+			<label for="chk" aria-hidden="true">Login</label>
+			<input type="email" name="email" placeholder="Email" required=""/>
+			{/* <Password/> */}
+			<button>Reset Password</button>
+			{/* <Link to="/" id="forgot-password" onClick={setForgot(forgot+1)}>Forgot password?</Link> */}
 </form>
-
-
-
 </div>
 
 </div>
-    );
+	);
+		}
+
+		else
+	console.log("Not forgot",{forgot});
+		return (
+
+		<div class= "login_page">
+
+				<input type="checkbox" id="chk" aria-hidden="true"/>
+		<Signup/>
+		<div className="login">
+
+		<form>
+							<label for="chk" aria-hidden="true">Login</label>
+							<input type="email" name="email" placeholder="Email" required=""/>
+
+							<input type="password" name="pswd" placeholder="Password" required=""/>
+							<button>Login</button>
+
+							<Link to="/" id="forgot-password" onClick={()=>setForgot(forgot+1)}>Forgot password?</Link>
+
+		</form>
+		</div>
+
+		</div>
+
+			);
+
+
+
 }
 export default Login
 
