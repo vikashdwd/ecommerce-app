704e294cc9203fb3cb9d1fd15f7e06494d19c80f
shivesh <vikashdwd428@gmail.com>
2023-02-24 22:27:14 +0530

    Signup page validation

--- src/Loginpage/Signup.js
+++ src/Loginpage/Signup.js
@@ -1,8 +1,18 @@
 import './Login.css';
 import dynButton from '../Elements/dynButton';
 import { motion } from 'framer-motion';
+import { useFormik } from 'formik';
 function Signup(){
-
+	const formik= useFormik({initialValues:{email:'',password:''},onSubmit: values=>{JSON.stringify(values,null,2)}});
+	const validate = (values) => {
+		const errors = {};
+		if (!values.email) {
+		  errors.email = 'Required';
+		} else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+		  errors.email = 'Invalid email address';
+		}
+		return errors;
+	};
     return(
 <div className= "Signup">
 	<form>
